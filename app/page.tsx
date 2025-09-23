8bce7c492aff779b19430dbdf6d317f02bf2385e
Jessie <jessieguo.05@gmail.com>
2025-09-23 15:30:38 +0100

    fix: imports and components

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
-import TabGroup from "@/components/TabGroup";
-import Header from "@/components/Header"
+import TabGroup from "@/components/tabs/TabGroup";
+import Header from "@/components/header/Header"
 import { Tab } from "@/types/Tab"
-import PanelGroup from "@/components/PanelGroup"
+import PanelGroup from "@/components/panels/PanelGroup"
 
 const tabData: Tab[] = [
   { key: 'student1', title: 'Amy', content: <PanelGroup></PanelGroup> },
--- components/panels/PanelGroup.tsx
+++ components/panels/PanelGroup.tsx
@@ -1,11 +1,15 @@
-import Panel from "@/components/Panel"
-import Resizer from "@/components/Resizer"
+"use client";
+
+import Panel from "@/components/panels/Panel";
+import Resizer from "@/components/panels/Resizer";
 import { useState } from "react";
 
-const [leftWidth, setLeftWidth] = useState(300); 
-const [isResizing, setIsResizing] = useState(false); 
+
 
 const PanelGroup: React.FC = () => {
+    const [leftWidth, setLeftWidth] = useState(300); 
+    const [isResizing, setIsResizing] = useState(false); 
+
     return (
         <div className="panel-group flex flex-row gap-4 h-screen"> 
             <div className="flex-1 p-4 text-center h-full " style={{ width: leftWidth }}><Panel></Panel></div>
